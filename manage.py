7fe61adcdf77a00f961174ca681239975a56e15b
Santtu Pajukanta <santtu@pajukanta.fi>
2015-09-14 22:51:27 +0300

    bail out early on python 2

--- manage.py
+++ manage.py
@@ -2,6 +2,8 @@
 import os
 import sys
 
+assert sys.version_info.major >= 3, "Edegal requires Python 3"
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edegal2.settings")
 
