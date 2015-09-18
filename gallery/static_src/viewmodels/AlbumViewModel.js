93f7b0017709810c8ef8e9dc999b7e7f60393c4e
Santtu Pajukanta <santtu@pajukanta.fi>
2015-09-18 14:24:04 +0300

    remove debug

--- gallery/static_src/viewmodels/AlbumViewModel.js
+++ gallery/static_src/viewmodels/AlbumViewModel.js
@@ -4,7 +4,5 @@ import ko from 'knockout';
 export default class AlbumViewModel {
   constructor() {
     this.album = ko.observable(null);
-
-    this.album.subscribe((newAlbum) => { console.log('album', newAlbum); })
   }
 }
