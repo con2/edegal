0438a44622544307a5e16cba4bfecf9a34920dec
Santtu Pajukanta <santtu@pajukanta.fi>
2015-09-19 00:05:21 +0300

    implement left/right keys

--- gallery/static_src/viewmodels/PictureViewModel.js
+++ gallery/static_src/viewmodels/PictureViewModel.js
@@ -1,8 +1,19 @@
+import _ from 'lodash';
 import Hammer from 'hammerjs';
 import ko from 'knockout';
 import page from 'page';
 
 import {selectMedia, preloadMedia, getOriginal} from '../helpers/MediaHelper';
+
+
+const PREV_PICTURE_KEYCODES = [
+        37, // left arrow
+        33, // page up
+      ],
+      NEXT_PICTURE_KEYCODES = [
+        39, // right arrow
+        34, // page down
+      ];
 
 
 export default class PictureViewModel {
@@ -16,6 +27,8 @@ export default class PictureViewModel {
     });
 
     this.picture.subscribe(this.preloadPrevNext, this);
+
+    this.setupKeyBindings();
   }
 
   goTo(prevNext) {
@@ -29,5 +42,19 @@ export default class PictureViewModel {
   preloadPrevNext(newPicture) {
     if (newPicture.next) { preloadMedia(newPicture.next); }
     if (newPicture.previous) { preloadMedia(newPicture.previous); }
+  }
+
+  setupKeyBindings() {
+    document.addEventListener('keydown', this.onKeyDown.bind(this), false);
+  }
+
+  onKeyDown(event) {
+    if (event.altKey || event.ctrlKey || event.metaKey) {
+      return true;
+    } else if (_.includes(NEXT_PICTURE_KEYCODES, event.keyCode)) {
+      this.goTo('next');
+    } else if (_.includes(PREV_PICTURE_KEYCODES, event.keyCode)) {
+      this.goTo('previous');
+    }
   }
 }
