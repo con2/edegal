eaec673dae01eb56a0d897df239b5f4742d6a28c
Santtu Pajukanta <santtu@pajukanta.fi>
2017-04-08 16:20:52 +0300

    frontend/helpers/preloadMedia: remove debug

--- frontend/src/helpers/preloadMedia.ts
+++ frontend/src/helpers/preloadMedia.ts
@@ -9,7 +9,6 @@ import selectMedia from './selectMedia';
  * @param picture The picture to preload media for.
  */
 export default function preloadMedia(picture: Picture) {
-  console.log('preloadMedia', picture.path);
   const media = selectMedia(picture);
   const element = document.createElement('img');
   element.src = media.src;
