a962ed74aff2bff0b62d75904a7b1012c9d1fa5c
Santtu Pajukanta <santtu@pajukanta.fi>
2017-04-12 23:30:45 +0300

    backend/celery: use shared_task after all

--- backend/edegal/tasks.py
+++ backend/edegal/tasks.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.core.mail import EmailMessage
 from django.core.management import call_command
 
-from edegal_site.celery import app
+from celery import shared_task
 
 from .models import Media, Picture, MediaSpec
 
@@ -12,7 +12,7 @@ from .models import Media, Picture, MediaSpec
 logger = logging.getLogger(__name__)
 
 
-@app.task(ignore_result=True)
+@shared_task(ignore_result=True)
 def send_email(**opts):
     if settings.DEBUG:
         logger.debug(opts['body'])
@@ -20,12 +20,12 @@ def send_email(**opts):
     EmailMessage(**opts).send(fail_silently=False)
 
 
-@app.task(ignore_result=True)
+@shared_task(ignore_result=True)
 def run_admin_command(*args, **kwargs):
     call_command(*args, **kwargs)
 
 
-@app.task(ignore_result=True)
+@shared_task(ignore_result=True)
 def import_local_media(picture_id, input_filename, mode, media_specs_ids, refresh_album):
     picture = Picture.objects.get(id=picture_id)
     media_specs = MediaSpec.objects.filter(id__in=media_specs_ids)
