e297682eae18232840d7eda038404ba5e6e066aa
Santtu Pajukanta <santtu@pajukanta.fi>
2018-09-10 20:21:35 +0300

    frontend/AlbumView: emlessen copy-paste

---
+++ frontend/src/components/AlbumView/AlbumGrid.tsx
@@ -0,0 +1,94 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+
+import TileItem from '../../models/TileItem';
+import { State } from '../../modules';
+import PictureTile from './PictureTile';
+
+
+const maxWidthFactor = 1.1;
+const scaleThresholdFactor = 0.8;
+const defaultThumbnailWidth = 240;
+const thumbnailHeight = 240;
+const borderAdjustmentPixels = 2;
+
+
+interface Row {
+  height: number;
+  totalWidth: number;
+  items: TileItem[];
+  scaleFactor: number;
+}
+
+
+const makeRow: () => Row = () => ({ height: thumbnailHeight, totalWidth: 0, items: [], scaleFactor: 1.0 });
+
+
+interface AlbumGridStateProps {
+  width: number;
+}
+interface AlbumGridOwnProps {
+  tiles: TileItem[];
+  getTitle(tile: TileItem): string;
+}
+type AlbumGridProps = AlbumGridStateProps & AlbumGridOwnProps;
+
+class AlbumGrid extends React.PureComponent<AlbumGridProps, {}> {
+  render() {
+    return (
+      <div>
+        {this.getRows(this.props.tiles).map((row, index) => (
+          <div key={index} className="AlbumView-row" style={{ height: Math.floor(row.height) + borderAdjustmentPixels }}>
+            {row.items.map((item) => (
+              <PictureTile
+                key={item.path}
+                path={item.path}
+                width={item.thumbnail ? item.thumbnail.width * row.scaleFactor : defaultThumbnailWidth * row.scaleFactor}
+                height={row.height}
+                title={item.title}
+                src={item.thumbnail ? item.thumbnail.src : undefined}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  private getRows(items: TileItem[]): Row[] {
+    let currentRow: Row = makeRow();
+    const rows: Row[] = [currentRow];
+    const maxWidth = maxWidthFactor * this.props.width;
+
+    items.forEach((tileItem) => {
+      const itemWidth = tileItem.thumbnail ? tileItem.thumbnail.width : defaultThumbnailWidth;
+
+      if (currentRow.totalWidth + itemWidth > maxWidth) {
+        // Initialize new row
+        currentRow = makeRow();
+        rows.push(currentRow);
+      }
+
+      currentRow.items.push(tileItem);
+      currentRow.totalWidth += itemWidth;
+    });
+
+    // Finalize rows
+    const scaleThreshold = this.props.width * scaleThresholdFactor;
+    rows.forEach((row) => {
+      if (row.totalWidth > scaleThreshold) {
+        row.scaleFactor = this.props.width / row.totalWidth;
+        row.height = thumbnailHeight * row.scaleFactor;
+      }
+    });
+
+    return rows;
+  }
+}
+
+
+const mapStateToProps = (state: State) => ({
+  width: state.mainView.width,
+});
+
+export default connect<AlbumGridStateProps, {}, AlbumGridOwnProps>(mapStateToProps)(AlbumGrid);
--- frontend/src/components/AlbumView/index.tsx
+++ frontend/src/components/AlbumView/index.tsx
@@ -6,31 +6,13 @@ import TileItem from '../../models/TileItem';
 import { State } from '../../modules';
 import AppBar from '../AppBar';
 
+import AlbumGrid from './AlbumGrid';
 import './index.css';
-import PictureTile from './PictureTile';
-
-
-const maxWidthFactor = 1.1;
-const scaleThresholdFactor = 0.8;
-const defaultThumbnailWidth = 240;
-const thumbnailHeight = 240;
-const borderAdjustmentPixels = 2;
 
 
 interface AlbumViewProps {
   album: Album;
-  width: number;
 }
-
-interface Row {
-  height: number;
-  totalWidth: number;
-  items: TileItem[];
-  scaleFactor: number;
-}
-
-
-const makeRow: () => Row = () => ({ height: thumbnailHeight, totalWidth: 0, items: [], scaleFactor: 1.0 });
 
 
 class AlbumView extends React.PureComponent<AlbumViewProps, {}> {
@@ -40,73 +22,19 @@ class AlbumView extends React.PureComponent<AlbumViewProps, {}> {
         <AppBar />
 
         {/* Subalbums */}
-        {this.getRows(this.props.album.subalbums).map((row, index) => (
-          <div key={index} className="AlbumView-row" style={{ height: Math.floor(row.height) + borderAdjustmentPixels }}>
-            {row.items.map((item) => (
-              <PictureTile
-                key={item.path}
-                path={item.path}
-                width={item.thumbnail ? item.thumbnail.width * row.scaleFactor : defaultThumbnailWidth * row.scaleFactor}
-                height={row.height}
-                title={item.title}
-                src={item.thumbnail ? item.thumbnail.src : undefined}
-              />
-            ))}
-          </div>
-        ))}
+        <AlbumGrid tiles={this.props.album.subalbums} getTitle={(tile: TileItem) => tile.title} />
 
         {/* Pictures */}
-        {this.getRows(this.props.album.pictures).map((row, index) => (
-          <div key={index} className="AlbumView-row" style={{ height: Math.floor(row.height) + borderAdjustmentPixels }}>
-            {row.items.map((item) => (
-              <PictureTile
-                key={item.path}
-                path={item.path}
-                width={item.thumbnail ? item.thumbnail.width * row.scaleFactor : defaultThumbnailWidth * row.scaleFactor}
-                height={row.height}
-                src={item.thumbnail ? item.thumbnail.src : undefined}
-              />
-            ))}
-          </div>
-        ))}
+        <AlbumGrid tiles={this.props.album.pictures} getTitle={(tile: TileItem) => ""} />
       </div>
     );
   }
 
-  private getRows(items: TileItem[]): Row[] {
-    let currentRow: Row = makeRow();
-    const rows: Row[] = [currentRow];
-    const maxWidth = maxWidthFactor * this.props.width;
 
-    items.forEach((tileItem) => {
-      const itemWidth = tileItem.thumbnail ? tileItem.thumbnail.width : defaultThumbnailWidth;
-
-      if (currentRow.totalWidth + itemWidth > maxWidth) {
-        // Initialize new row
-        currentRow = makeRow();
-        rows.push(currentRow);
-      }
-
-      currentRow.items.push(tileItem);
-      currentRow.totalWidth += itemWidth;
-    });
-
-    // Finalize rows
-    const scaleThreshold = this.props.width * scaleThresholdFactor;
-    rows.forEach((row) => {
-      if (row.totalWidth > scaleThreshold) {
-        row.scaleFactor = this.props.width / row.totalWidth;
-        row.height = thumbnailHeight * row.scaleFactor;
-      }
-    });
-
-    return rows;
-  }
 }
 
 
 const mapStateToProps = (state: State) => ({
-  width: state.mainView.width,
   album: state.album,
 });
 
