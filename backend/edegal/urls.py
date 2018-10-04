741e5f24d30d6cce12a7f524334437015a4c42c7
Santtu Pajukanta <santtu@pajukanta.fi>
2018-10-04 10:49:25 +0300

    backend: remove trailing slash eg. /api/v3/mimicon-2018/

--- backend/edegal/urls.py
+++ backend/edegal/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from .views import api_v3_view
 
 urlpatterns = [
-    url(r'^api/v3(?P<path>[a-z0-9/-]+)$', api_v3_view, name='api_v3_view'),
+    url(r'^api/v3(?P<path>[a-z0-9/-]+?)/?$', api_v3_view, name='api_v3_view'),
 ]
