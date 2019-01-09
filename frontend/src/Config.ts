8f4f81e0a77f42db1e24feb54a54c63a3f1cd744
Santtu Pajukanta <santtu@pajukanta.fi>
2019-01-09 20:38:16 +0200

    frontend: please do not redirect larppikuvat.fi and dev.conikuvat.fi to conikuvat.fi when logging in

--- frontend/src/Config.ts
+++ frontend/src/Config.ts
@@ -11,7 +11,7 @@ const Config = {
     baseUrl: process.env.REACT_APP_EDEGAL_BACKEND_URL || '',
     apiPrefix: '/api/v3',
   },
-  loginUrl: process.env.REACT_APP_EDEGAL_LOGIN_URL || 'http://localhost:8000/admin/',
+  loginUrl: process.env.REACT_APP_EDEGAL_LOGIN_URL || '/admin/oauth2/login/?next=/admin/',
 };
 
 
