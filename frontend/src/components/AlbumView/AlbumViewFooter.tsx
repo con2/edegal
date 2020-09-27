f02a0a53b62526296f18106fcdd7939951c99322
Santtu Pajukanta <santtu@pajukanta.fi>
2020-09-27 21:10:58 +0300

    frontend/AlbumViewFooter: emsymmetrify

--- frontend/src/components/AlbumView/AlbumViewFooter.tsx
+++ frontend/src/components/AlbumView/AlbumViewFooter.tsx
@@ -22,7 +22,7 @@ const AlbumViewFooter: React.FC<FooterProps> = ({ album }) => {
           {t(r => r.albumCopyright)} &copy; {getYear(album)} <Link to={photographer.path}>{photographer.display_name}</Link>.{' '}
         </>
       ) : null}
-      <a href="https://github.com/conikuvat/edegal">Edegal</a> &copy; 2010–2020 Santtu Pajukanta.
+      Edegal &copy; 2010–2020 <a href="https://github.com/conikuvat/edegal">Santtu Pajukanta</a>.
     </footer>,
     document.body,
   );
